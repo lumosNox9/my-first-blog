7c9567df9da2e3f2aa664ee16ab6ce6b12c29286
lumosNox9 <justabeagle@gmail.com>
2020-10-28 21:06:49 -0500

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
